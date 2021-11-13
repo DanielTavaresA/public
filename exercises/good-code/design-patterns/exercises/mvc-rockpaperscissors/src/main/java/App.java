4657c05b6344313562e57222674a19b13c70c050
Daniel.T <comptesdaniel@gmail.com>
2021-11-13 21:56:54 +0100

    goodcode

--- exercises/good-code/design-patterns/exercises/mvc-rockpaperscissors/src/main/java/App.java
+++ exercises/good-code/design-patterns/exercises/mvc-rockpaperscissors/src/main/java/App.java
@@ -1,7 +1,17 @@
+import controller.Controller;
+import model.Model;
+import view.TextView;
+import view.View;
+
 public class App {
 
   /** Starts the "Rock Paper Scissors" game */
   public static void main(String[] args) {
     // TODO: instantiate MVC components so you can play the game
+    Model model = new Model();
+    Controller controller = new Controller(model);
+    View view = new TextView(model, controller);
+
+
   }
 }
